80803fc690772a87acdb825449e8f658303ae0dc
Manu Diaz <diazev20@gmail.com>
2025-04-16 16:30:16 -0500

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WebhookModule } from './webhook/webhook.module';
 import { ServicesService } from './services/services.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { ServicesService } from './services/services.service';
 
 @Module({
-  imports: [WebhookModule],
+  imports: [WebhookModule, PrismaModule],
   providers: [ServicesService],
 })
 export class AppModule {}

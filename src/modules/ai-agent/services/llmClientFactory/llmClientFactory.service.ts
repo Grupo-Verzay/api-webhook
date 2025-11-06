051d09de038776acf39d6f941bb20c65b576b674
Eros Hervilla <cupidreborn522@gmail.com>
2025-11-06 11:51:50 -0400

    quitar comentarios

--- src/modules/ai-agent/services/llmClientFactory/llmClientFactory.service.ts
+++ src/modules/ai-agent/services/llmClientFactory/llmClientFactory.service.ts
@@ -16,9 +16,7 @@ export class LlmClientFactory {
   public getClient<P extends Provider>(config: ModelConfig<P>) {
     const { provider, model, apiKey } = config
     const clientKey = `${provider}:${model}`;
-    // if (this.clients.has(clientKey)) {
-    //   return this.clients.get(clientKey)!;
-    // }
+
 
     const client = (() => {
       switch (provider) {
@@ -35,11 +33,6 @@ export class LlmClientFactory {
           throw new Error(`Unsupported LLM provider: ${provider}`);
       }
     })();
-    // this.clients.set(clientKey, client);
-    // const bindR = client.bindTools(langchainTools)
-    // console.log('BindR is', bindR)
-    // bindR.invoke('comunicame con un asesor por favor').then((data) => console.log("Debuggin tools... 😎", JSON.stringify(data, null, 2)))
-
     return client;
   }
 }

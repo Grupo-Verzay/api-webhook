cccc1fff2ab89b8562763d8bf6d2f8276ccd7769
Eros Hervilla <cupidreborn522@gmail.com>
2025-11-08 12:20:49 -0400

    historial en postRepuesta de 2 mensajes

--- src/modules/ai-agent/ai-agent.service.ts
+++ src/modules/ai-agent/ai-agent.service.ts
@@ -117,21 +117,22 @@ ${followupText}`
     const styleLock = new SystemMessage({
       content: [{
         type: 'text',
-        text: `
-SALIDA:
-- Responde SIEMPRE con texto natural en español, puedes usar emojis o negrita.
-- PROHIBIDO: JSON, objetos, arrays, backticks, bloques de código o etiquetas.
-- Si tu salida empezaría con "{" o "[", reescríbela como texto llano.`
+        text:''
+//         text: `
+// SALIDA:
+// - Responde SIEMPRE con texto natural en español, puedes usar emojis o negrita.
+// - PROHIBIDO: JSON, objetos, arrays, backticks, bloques de código o etiquetas.
+// - Si tu salida empezaría con "{" o "[", reescríbela como texto llano.`
       }]
     });
 
-    const historyMessages = chatHistory.map(text => new HumanMessage({ content: [{ type: "text", text }] }));
+    const historyMessages = chatHistory.map(text => new HumanMessage({ content: [{ type: "text", text }] })).splice(0,2);
     const rawUser = new HumanMessage({ content: [{ type: 'text', text: userPrompt }] });
 
     const completion = await this.aiClient.invoke([
       systemMessage,
       styleLock,           // 👈 ponlo después del prompt principal
-      // ...historyMessages,
+      ...historyMessages,
       rawUser,
     ]);
 

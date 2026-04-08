c625c09681162c01099c4f0fd20685ddd4c1e7f7
diazEmanuel21 <diazev20@gmail.com>
2026-04-08 11:16:41 -0500

    fix: err notify

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ServicesService } from './services/services.service';
-import { SessionService } from './modules/session/session.service';
 import { LoggerModule } from './core/logger/logger.module';
 import { WebhookModule } from './modules/webhook/webhook.module';
 import configuration from './config/configuration';
 import { DatabaseModule } from './database/database.module';
-import { InstancesService } from './modules/instances/instances.service';
 import { AiAgentModule } from './modules/ai-agent/ai-agent.module';
-import { UserService } from './modules/user/user.service';
-import { ChatHistoryService } from './modules/chat-history/chat-history.service';
-import { SeguimientosService } from './modules/seguimientos/seguimientos.service';
-import { AutoRepliesService } from './modules/auto-replies/auto-replies.service';
-import { NotificacionToolService } from './modules/ai-agent/tools/notificacion/notificacion.service';
-import { AiCreditsService } from './modules/ai-credits/ai-credits.service';
-import { SessionTriggerService } from './modules/session-trigger/session-trigger.service';
 import { LeadFunnelModule } from './modules/lead-funnel/lead-funnel.module';
 import { RegistrosModule } from './modules/registros/registros.module';
 import { ChatHistoryModule } from './modules/chat-history/chat-history.module';
@@ -35,15 +26,6 @@ import { ChatHistoryModule } from './modules/chat-history/chat-history.module';
   ],
   providers: [
     ServicesService,
-    SessionService,
-    InstancesService,
-    UserService,
-    ChatHistoryService,
-    SeguimientosService,
-    AutoRepliesService,
-    NotificacionToolService,
-    AiCreditsService,
-    SessionTriggerService,
   ],
 })
 export class AppModule {}
--- src/modules/webhook/webhook.module.ts
+++ src/modules/webhook/webhook.module.ts
@@ -7,19 +7,14 @@ import { SessionService } from 'src/modules/session/session.service';
 import { MessageDirectionService } from './services/message-direction/message-direction.service';
 import { MessageTypeHandlerService } from './services/message-type-handler/message-type-handler.service';
 import { InstancesService } from '../instances/instances.service';
-import { AiAgentService } from '../ai-agent/ai-agent.service';
 import { PromptService } from '../prompt/prompt.service';
 import { UserService } from '../user/user.service';
 import { MessageBufferService } from './services/message-buffer/message-buffer.service';
 import { ChatHistoryService } from '../chat-history/chat-history.service';
 import { WorkflowModule } from '../workflow/workflow.module';
 import { IntentionService } from '../ai-agent/services/intention/intention.service';
-import { NodeSenderService } from '../workflow/services/node-sender.service.ts/node-sender.service';
 import { SeguimientosService } from '../seguimientos/seguimientos.service';
 import { AutoRepliesService } from '../auto-replies/auto-replies.service';
-import { WorkflowService } from '../workflow/services/workflow.service.ts/workflow.service';
-import { NotificacionToolService } from '../ai-agent/tools/notificacion/notificacion.service';
-import { AiCreditsService } from '../ai-credits/ai-credits.service';
 import { WebhookControlService } from './services/webhook-control/webhook-control.service';
 import { SessionTriggerService } from '../session-trigger/session-trigger.service';
 import { AntifloodService } from './services/antiflood/antiflood.service';
@@ -45,12 +40,8 @@ import { BillingCronSchedulerService } from './services/billing-cron/billing-cro
     ChatHistoryService,
     IntentionService,
     PromptCompressorService,
-    NodeSenderService,
     SeguimientosService,
     AutoRepliesService,
-    WorkflowService,
-    NotificacionToolService,
-    AiCreditsService,
     SessionTriggerService,
     // pertenece
     WebhookService,
